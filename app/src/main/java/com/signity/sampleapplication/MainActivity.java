fad8efae50964affdd4989d513e13329fdf92629
priya <priya.preet@signitysolutions.in>
2018-06-26 16:42:55 +0530

    First commit

---
+++ app/src/main/java/com/signity/sampleapplication/MainActivity.java
@@ -0,0 +1,87 @@
+package com.signity.sampleapplication;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.PointF;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+import java.security.Permission;
+
+public class MainActivity extends AppCompatActivity {
+
+    QRCodeReaderView qrCodeReaderView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        askCameraPermission();
+    }
+
+    private void initScanner() {
+
+        qrCodeReaderView = findViewById(R.id.qrCodeReader);
+        // Use this function to enable/disable decoding
+        qrCodeReaderView.setQRDecodingEnabled(true);
+
+       /* // Use this function to change the autofocus interval (default is 5 secs)
+        qrCodeReaderView.setAutofocusInterval(2000L);
+
+        // Use this function to enable/disable Torch
+        qrCodeReaderView.setTorchEnabled(true);
+
+        // Use this function to set front camera preview
+        qrCodeReaderView.setFrontCamera();
+
+        qrCodeReaderView.setBackCamera();*/
+
+        qrCodeReaderView.setOnQRCodeReadListener(new QRCodeReaderView.OnQRCodeReadListener() {
+            @Override
+            public void onQRCodeRead(String text, PointF[] points) {
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+                //qrCodeReaderView.setQRDecodingEnabled(false);
+            }
+        });
+    }
+
+    private void askCameraPermission() {
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String []{Manifest.permission.CAMERA}, 101);
+        }
+        else{
+            initScanner();
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        qrCodeReaderView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        qrCodeReaderView.stopCamera();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 101){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                initScanner();
+            }
+        }
+    }
+}
